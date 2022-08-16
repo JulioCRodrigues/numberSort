21cf720ce250faa6358e3491e3bfc3b13bf057d1
jrodrigues <julio.rodrigues@foursys.com.br>
2022-08-16 14:56:35 -0300

    Aplicativo de sorteio com números randomicos

---
+++ app/src/main/java/com/julioprojects/sorteio/MainActivity.java
@@ -0,0 +1,39 @@
+package com.julioprojects.sorteio;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
+
+import com.julioprojects.sorteio.databinding.ActivityMainBinding;
+
+import java.util.Random;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        setContentView(binding.getRoot());
+
+        binding.tvNumberSort.setText("-");
+
+        sortearNumero();
+
+    }
+
+    private void sortearNumero() {
+        binding.buttonSort.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Random random = new Random();
+                int intervalo = random.nextInt(10);
+                binding.tvNumberSort.setText(String.valueOf(intervalo));
+            }
+        });
+    }
+}
